27aa8ab823a5303bf99b10b59f26b212785e63a0
francoax <francoa.duarte2001@gmail.com>
2022-09-18 21:56:40 -0300

    interfaces

---
+++ EjerciciosTS-Interfaces/ej2-2.ts
@@ -0,0 +1,12 @@
+interface AccesibleProps {
+      nombre : string;
+      edad : number;
+}
+
+class Persona implements AccesibleProps {
+    constructor(public nombre: string, public edad: number) { }
+}
+
+const juan = new Persona('Juan', 31);
+
+console.log('[Ejercicio 2.2]', `${juan.nombre} tiene ${juan.edad} anios.`);
